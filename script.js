b3ca212cd74979e54003f0d4755264f1e4f5d668
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-20 14:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function threeSum(arr, target) {
 // write your code here
+	let n = arr.length;
+	for (let i = 0; i < n-2; i++) {
+		for (let j = i+1; j < n-1; j++) {
+			for (let k = 0; k < n; k++) {
+				if (arr[i] + arr[j] + arr[k] === target) {
+					return arr[i] + arr[j] + arr[k];
+				}
+			}
+		}
+	}
   
 }
 
