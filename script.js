d64889e5712327d4da5ca2f1bc70d160d4ee1ceb
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-20 16:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-function threeSum(arr, target) {
-// write your code here
-	let n = arr.length;
-	let initialDiff = Infinity, ans = -1;
-	for (let i = 0; i < n-2; i++) {
-		for (let j = i+1; j < n-1; j++) {
-			for (let k = j+1; k < n; k++) {
-				let sum = arr[i] + arr[j] + arr[k];
-				let diff = Math.abs(sum - target)
-				if (initialDiff > diff) {
-					initialDiff = diff;
-					ans = sum;
-				}
+// function threeSum(arr, target) {
+// // write your code here
+// 	let n = arr.length;
+// 	let initialDiff = Infinity, ans = -1;
+// 	for (let i = 0; i < n-2; i++) {
+// 		for (let j = i+1; j < n-1; j++) {
+// 			for (let k = j+1; k < n; k++) {
+// 				let sum = arr[i] + arr[j] + arr[k];
+// 				let diff = Math.abs(sum - target)
+// 				if (initialDiff > diff) {
+// 					initialDiff = diff;
+// 					ans = sum;
+// 				}
 				
 				
-				// if (arr[i] + arr[j] + arr[k] === target) {
-				// 	return arr[i] + arr[j] + arr[k];
-				// }
-			}
-		}
-	}
-	console.log(ans);
+// 				// if (arr[i] + arr[j] + arr[k] === target) {
+// 				// 	return arr[i] + arr[j] + arr[k];
+// 				// }
+// 			}
+// 		}
+// 	}
+// 	console.log(ans);
   
+// }
+
+// module.exports = threeSum;
+
+
+function threeSum(arr, target) {
+  let n = arr.length;
+  let initialDiff = Infinity;
+  let closestSum = null;
+
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        let sum = arr[i] + arr[j] + arr[k];
+        let diff = Math.abs(sum - target);
+
+        if (diff < initialDiff) {
+          initialDiff = diff;
+          closestSum = sum;
+        }
+      }
+    }
+  }
+  
+  return closestSum;
 }
 
 module.exports = threeSum;
