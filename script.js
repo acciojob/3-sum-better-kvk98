d0d93e5f228afba9a371256189730eb5b0ad2638
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-20 15:17:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function threeSum(arr, target) {
 // write your code here
 	let n = arr.length;
+	let initialDiff = Infinity, ans = -1;
 	for (let i = 0; i < n-2; i++) {
 		for (let j = i+1; j < n-1; j++) {
 			for (let k = 0; k < n; k++) {
-				if (arr[i] + arr[j] + arr[k] === target) {
-					return arr[i] + arr[j] + arr[k];
+				let sum = arr[i] + arr[j] + arr[k];
+				let diff = Math.abs(sum - target)
+				if (initialDiff > diff) {
+					ans = sum;
 				}
+				
+				
+				// if (arr[i] + arr[j] + arr[k] === target) {
+				// 	return arr[i] + arr[j] + arr[k];
+				// }
 			}
 		}
 	}
+	console.log(ans);
   
 }
 
